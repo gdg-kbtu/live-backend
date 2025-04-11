425560eb4a54340571269ce7ecd76bc94887992c
Altair <a_zharkimbayev@kbtu.kz>
2025-04-11 16:21:22 +0500

    created template 'index.html' for 'main' app. The url '' now leads to index.html

--- site/main/views.py
+++ site/main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("<h4>Проверка работы</h4>")
+  return render(request, 'main/index.html')
