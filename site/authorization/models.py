61d2ce9a4db40fc872151ab4ab3094b6ec3e8a57
Altair <assasinaltair228@gmail.com>
2025-04-20 22:21:07 +0500

    created models for github projects and their commits. Connected User and Project tables with many-to-many relation. Connected Project and Commit with one-to-many relation.

--- site/authorization/models.py
+++ site/authorization/models.py
@@ -7,4 +7,19 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     studentID = models.CharField(max_length=10, unique=True)
     position = models.CharField(max_length=10, choices=[('mentor', 'Mentor'), ('student', 'Student')])
-    mentor = models.ForeignKey('self', related_name='students', on_delete=models.SET_NULL, null=True, blank=True, limit_choices_to={'position': 'mentor'})
+    mentor = models.ForeignKey('self', to_field='studentID', related_name='students', on_delete=models.SET_NULL, null=True, blank=True, limit_choices_to={'position': 'mentor'})
+    project = models.ManyToManyField('authorization.Project', blank=True, related_name='contributors')
+
+
+class Project(models.Model):
+    projectID = models.CharField(unique=True)
+    name = models.CharField()
+    description = models.CharField(null=True, blank=True)
+    github_url = models.CharField()
+
+class Commit(models.Model):
+    commitID = models.CharField(unique=True)
+    details = models.CharField(null=True, blank=True)
+    commitAuthor = models.CharField()
+    project = models.ForeignKey('authorization.Project', to_field='projectID', on_delete=models.SET_NULL, null=True, blank=True, related_name='commits')
+    
